2982f183b7203af55372f6c63ccb7cbef9115d59
nguyenvanlap1 <nguyenvanlap3965@gmail.com>
2024-11-17 20:37:15 +0700

    hi

--- app/controllers/nhaxuatban.controller.js
+++ app/controllers/nhaxuatban.controller.js
@@ -1,55 +1,47 @@
-exports.create = async (req, res, next) => {
+const MongoDB = require('../utils/mongodb.util');
+const NhaXuatBanService = require('../services/nhaxuatban.service');
+const ApiError = require('../api-error');
+
+exports.create = async (req, res, next) => { 
     try {
-        res.send({ message: "create NXB" });
-    } catch (error) {
-        next(error);
+        const nhaxuatbanService = new NhaXuatBanService(MongoDB.client); 
+        const result = await nhaxuatbanService.create(req.body); 
+        if (result.error) { 
+            return next(
+                new ApiError(400, result.error)
+            )
+        } else { 
+            res.status(201).send(result); 
+        } 
+    } 
+    catch (error) { 
+        return next(
+            new ApiError(500, 'An error occurred while creating nxb')
+        );
     }
-};
+}
 
 exports.findAll = async (req, res, next) => {
-    try {
-        res.send({ message: "findAll NXB" });
-    } catch (error) {
-        next(error);
-    }
+    res.send({message: "find all nhaxuatban"})
 };
 
 exports.findOne = async (req, res, next) => {
-    try {
-        res.send({ message: "findOne NXB" });
-    } catch (error) {
-        next(error);
-    }
+    res.send({message: "find one nhaxuatban"})
 };
 
 exports.update = async (req, res, next) => {
-    try {
-        res.send({ message: "update NXB" });
-    } catch (error) {
-        next(error);
-    }
+    res.send({message: "update nhaxuatban"})
 };
 
 exports.delete = async (req, res, next) => {
-    try {
-        res.send({ message: "delete NXB" });
-    } catch (error) {
-        next(error);
-    }
+    res.send({message: "delete nhaxuatban"})
 };
 
-exports.deleteAll = async (req, res, next) => {
-    try {
-        res.send({ message: "deleteAll NXB" });
-    } catch (error) {
-        next(error);
-    }
+exports.deleteAll = async (req, res, next) =>  {
+    res.send({message: "delete all nhaxuatban"})
 };
 
 exports.findAllFavorite = async (req, res, next) => {
-    try {
-        res.send({ message: "findAllFavorite NXB" });
-    } catch (error) {
-        next(error);
-    }
+    res.send({message: "find all favorite nhaxuatban"})
 };
+// file controller

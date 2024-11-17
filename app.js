efc28df7b7977feb8bface049e6f33872251d0bd
Chau Trong Phuc <fut.chautrongphuc@gmail.com>
2024-11-17 19:36:21 +0700

    controllers

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ app.get('/', (req, res) => {
     res.json({ message: "Welcome to contactbook aplication." });
 });
 
-app.use('/api/contacts', contactsRouter);
-
 app.use('/api/nhaxuatban', nhaxuatbanRouter);
 app.use('/api/sach', sachRouter);
 app.use('/api/theodoimuonsach', sachRouter);
 app.use('/api/nhanvien', nhaxuatbanRouter);
 app.use('/api/docgia', nhaxuatbanRouter);
-
 
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));

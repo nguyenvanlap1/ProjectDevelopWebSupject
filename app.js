fee82b87948b7b69cad42f8d746b1d196cee52d3
nguyenvanlap1 <nguyenvanlap3965@gmail.com>
2024-11-17 19:50:23 +0700

    route

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const ApiError = require('./app/api-error');
 const nhaxuatbanRouter = require('./app/routes/nhaxuatban.route');
-// const sachRouter = require('./app/routes/sach.route');
-// const theodoimuonsachRouter = require('./app/routes/theodoimuonsach.route');
-// const nhanvienRouter = require('./app/routes/nhanvien.route');
-// const docgiaRouter = require('./app/routes/docgia.route');
+const sachRouter = require('./app/routes/sach.route');
+const theodoimuonsachRouter = require('./app/routes/theodoimuonsach.route');
+const nhanvienRouter = require('./app/routes/nhanvien.route');
+const docgiaRouter = require('./app/routes/docgia.route');
 
 const app = express();
 
@@ -16,11 +16,11 @@ app.get('/', (req, res) => {
     res.json({ message: "Welcome to contactbook application." });
 });
 
-// app.use('/api/nhaxuatban', nhaxuatbanRouter);
-// app.use('/api/sach', sachRouter);
-// app.use('/api/theodoimuonsach', theodoimuonsachRouter);
-// app.use('/api/nhanvien', nhanvienRouter);
-// app.use('/api/docgia', docgiaRouter);
+app.use('/api/nhaxuatban', nhaxuatbanRouter);
+app.use('/api/sach', sachRouter);
+app.use('/api/theodoimuonsach', theodoimuonsachRouter);
+app.use('/api/nhanvien', nhanvienRouter);
+app.use('/api/docgia', docgiaRouter);
 
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
